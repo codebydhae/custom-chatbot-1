66292d8f123f24b89504b4fab82dab4c81da1ace
“codebydhae” <dhaesha3000@gmail.com>
2025-01-15 00:12:21 -0500

    bot logic complete. will troubleshoot errors.

--- main.py
+++ main.py
@@ -40,5 +40,14 @@ def chat_bot():
         if best_match:
             answer: str = get_answer_for_question(best_match, knowledge_base)
             print(f'Bot: {answer}')
+        else:
+            print('Bot: I don\'t know what to say. Can you teach me?')
+            new_answer: str = input('Type the answer or "skip" to skip:   ')
 
+            if new_answer.lower() == 'skip':
+                knowledge_base["questions"].append({"question":  user_input, "answer": new_answer})
+                save_knowledge_base('knowledge_base.json',  knowledge_base)
+                print('Bot: Thank you. Now I know!')
 
+if __name__ == '__main__':
+    chat_bot()
